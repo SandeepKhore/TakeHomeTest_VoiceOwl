d1ccba510834e50f9199e0ccf7e1f0a28b46b881
Sandeep Khore <skhore@taskhuman.com>
2025-11-25 14:56:21 +0530

    change DB config structure

---
+++ src/config/db.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import logger from "../utils/logger";
+
+export async function connectDB() {
+  const uri = process.env.MONGO_URI || "mongodb://localhost:27017/voice_owl";
+  try {
+    await mongoose.connect(uri);
+    logger.log("MongoDB connected");
+  } catch (err) {
+    logger.error("MongoDB connection error:", err);
+    process.exit(1);
+  }
+}
