1e242cd572851589e2ad8152bb1d715243c6721e
Sandeep Khore <skhore@taskhuman.com>
2025-11-25 13:16:48 +0530

    adding retry

--- src/controllers/transcription.controller.ts
+++ src/controllers/transcription.controller.ts
@@ -1,11 +1,14 @@
 import { Request, Response } from "express";
+import { CreateTranscriptionRequest, CreateTranscriptionResponse } from "../types/transcription.types";
 import { processTranscription } from "../services/transcription.service";
 import logger from "../utils/logger";
 
-export async function createTranscription(req: Request, res: Response) {
+export async function createTranscription(req: Request<{}, {}, CreateTranscriptionRequest>, res: Response) {
   try {
     const record = await processTranscription(req.body.audioUrl);
-    res.status(201).json({ _id: record._id });
+
+    const response: CreateTranscriptionResponse = { _id: record._id.toString() }
+    res.status(201).json(response);
   } catch (err: any) {
     logger.error('Create transcription failed', err)
     res.status(400).json({ error: err.message });
